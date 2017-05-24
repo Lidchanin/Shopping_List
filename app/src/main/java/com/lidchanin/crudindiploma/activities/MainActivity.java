dbbad2604a209e80025e9e963671f37d290b7fdf
Gldestroed <gldestroyed@gmail.com>
2017-05-24 11:55:01 +0300

    Updated Readme,add sing in google

--- app/src/main/java/com/lidchanin/crudindiploma/activities/MainActivity.java
+++ app/src/main/java/com/lidchanin/crudindiploma/activities/MainActivity.java
@@ -10,12 +10,27 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.crash.FirebaseCrash;
+import com.google.firebase.storage.FirebaseStorage;
 import com.lidchanin.crudindiploma.Constants;
 import com.lidchanin.crudindiploma.R;
 import com.lidchanin.crudindiploma.utils.DownloadTask;
@@ -23,14 +38,19 @@ import com.lidchanin.crudindiploma.utils.SharedPrefsManager;
 
 import java.io.File;
 
-public class MainActivity extends AppCompatActivity {
-    //// TODO: 21.05.2017 Log in google at start!!!!!!!!!!
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
+    private static final int RC_SIGN_IN = 9001;
+    //// TODO: 21.05.2017 Add analytics and Crush report!
     //// TODO: 22.05.2017  fix all layout
     private Button buttonRus;
     private Button buttonEng;
     private ProgressBar progressEng;
     private ProgressBar progressRus;
     private Button buttonGoToCam;
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
+    private FirebaseAuth mAuth;
     private static final int REQUEST_CAMERA_PERMISSION = 200;
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -45,8 +65,23 @@ public class MainActivity extends AppCompatActivity {
         progressEng = (ProgressBar) findViewById(R.id.eng_tessdata_progress);
         progressRus = (ProgressBar) findViewById(R.id.rus_tessdata_progress);
         buttonGoToCam = (Button) findViewById(R.id.to_camera);
+        signInButton = (SignInButton) findViewById(R.id.sing_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
         progressRus.setVisibility(View.GONE);
         progressEng.setVisibility(View.GONE);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
         if(Build.VERSION.SDK_INT>=21){
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CAMERA_PERMISSION);
@@ -73,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+
         buttonRus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -130,5 +166,49 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean(Constants.SharedPreferences.PREF_KEY_SHORTCUT_ADDED, true);
         editor.apply();
+    }
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+            }
+        }
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
