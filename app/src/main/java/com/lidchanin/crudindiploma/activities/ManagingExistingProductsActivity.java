65efc881159d44bfde2a12f6aeb6ef4c98df6aa5
Gldestroed <gldestroyed@gmail.com>
2017-06-08 09:24:59 +0300

    navigation drawer fixed

--- app/src/main/java/com/lidchanin/crudindiploma/activities/ManagingExistingProductsActivity.java
+++ app/src/main/java/com/lidchanin/crudindiploma/activities/ManagingExistingProductsActivity.java
@@ -1,21 +1,35 @@
 package com.lidchanin.crudindiploma.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.lidchanin.crudindiploma.R;
 import com.lidchanin.crudindiploma.adapters.ManagingExistingProductsRecyclerViewAdapter;
 import com.lidchanin.crudindiploma.data.dao.ProductDAO;
+import com.lidchanin.crudindiploma.data.dao.ShoppingListDAO;
 import com.lidchanin.crudindiploma.data.models.Product;
+import com.makeramen.roundedimageview.RoundedTransformationBuilder;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Transformation;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +51,20 @@ public class ManagingExistingProductsActivity extends AppCompatActivity
 
     private List<Product> products;
 
+    private ImageButton buttonHamburger;
+
+
+
+    private DrawerLayout drawer;
+    private Uri photoUrl;
+    private String accountName;
+    private String accountEmail;
+    private FirebaseUser currentUser ;
+    private FirebaseAuth mAuth;
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private ImageView headerImageView;
+    private Transformation transformation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +80,14 @@ public class ManagingExistingProductsActivity extends AppCompatActivity
             products = new ArrayList<>();
         }
 
+        buttonHamburger=(ImageButton) findViewById(R.id.hamburger);
+        initNavigationDrawer();
+        buttonHamburger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.LEFT);
+            }
+        });
         initializeRecyclerView();
         initializeAdapter();
     }
@@ -87,6 +123,22 @@ public class ManagingExistingProductsActivity extends AppCompatActivity
         recyclerViewAllProducts.setAdapter(adapter);
     }
 
+     void initNavigationDrawer() {
+        mAuth = FirebaseAuth.getInstance();
+        transformation = new RoundedTransformationBuilder().oval(true).build();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header_main);
+        headerImageView =(ImageView) headerLayout.findViewById(R.id.headerImageView);
+        emailTextView =(TextView) headerLayout.findViewById(R.id.user_mail);
+        nameTextView =(TextView) headerLayout.findViewById(R.id.user_name);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, null,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        navigationView.setNavigationItemSelectedListener(this);
+        toggle.syncState();
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
@@ -112,5 +164,20 @@ public class ManagingExistingProductsActivity extends AppCompatActivity
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser!=null){
+            assert currentUser != null;
+            photoUrl=currentUser.getPhotoUrl();
+            accountName = currentUser.getDisplayName();
+            accountEmail = currentUser.getEmail();
+            emailTextView.setText(accountEmail);
+            nameTextView.setText(accountName);
+            Picasso.with(getApplicationContext()).load(photoUrl).transform(transformation).into(headerImageView);
+        }
     }
 }
