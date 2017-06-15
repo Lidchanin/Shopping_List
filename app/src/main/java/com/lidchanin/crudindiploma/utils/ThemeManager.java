0d368bc8dd9e9afed122d55e13a2b582dd1cf1ba
Gldestroed <gldestroyed@gmail.com>
2017-06-15 17:21:01 +0300

    fix old devices colors and main menu

--- app/src/main/java/com/lidchanin/crudindiploma/utils/ThemeManager.java
+++ app/src/main/java/com/lidchanin/crudindiploma/utils/ThemeManager.java
@@ -1,6 +1,7 @@
 package com.lidchanin.crudindiploma.utils;
 
 import android.content.Context;
+import android.os.Build;
 
 import com.lidchanin.crudindiploma.Constants;
 import com.lidchanin.crudindiploma.R;
@@ -8,11 +9,14 @@ import com.lidchanin.crudindiploma.R;
 public class ThemeManager {
     public ThemeManager(Context context){
         final String prefOutput =new SharedPrefsManager(context).readString(Constants.SharedPreferences.PREF_KEY_THEME);
-        if(prefOutput.equals("blue")){
-            context.setTheme(R.style.BlueGradientTheme);
-        }
-        if(prefOutput.equals("purple")){
-            context.setTheme(R.style.PurpleGradientTheme);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+            if (prefOutput.equals("blue")) {
+                context.setTheme(R.style.BlueGradientTheme);
+            }
+            if (prefOutput.equals("purple")) {
+                context.setTheme(R.style.PurpleGradientTheme);
+            }
         }
     }
 }
